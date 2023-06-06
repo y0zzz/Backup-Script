fe9c145483af032f8a93c3c64cbe3696e79b55c8
Yosuf <116045258+y0zzz@users.noreply.github.com>
2023-06-07 00:02:03 +0200

    Update backupscript.sh

--- backupscript.sh
+++ backupscript.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+# Check if two arguments are provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <source_directory> <target_directory>"
+    exit 1
+fi
+
 # Source directory to be backed up
-SOURCE_DIR=/Users/yuzz/Documents/Devops
-
+SOURCE_DIR=$1 
 # Target directory for backups (local or remote)
-TARGET_DIR=/Users/yuzz/Documents/Devops
-
-# SSH connection details for remote backup (uncomment and update if applicable)
- SSH_USER="yuzz"
- SSH_HOST="yserver" 
+TARGET_DIR=$2 
 
 # Function to create timestamped directory for backup
 create_backup_directory() {
@@ -26,18 +27,15 @@ create_backup_directory() {
 
 # Function to perform incremental backup
 perform_backup() {
-    local backup_dir="$TARGET_DIR/latest"
-    
-    rsync -avz --link-dest="$backup_dir" "$SOURCE_DIR" "$TARGET_DIR/in_progress"
-    mv "$TARGET_DIR/in_progress" "$backup_dir"
+    # backup with rsync
+    if [ "$remote" = true ]; then
+    rsync -avz --delete --link-dest="$TGT/latest" -e ssh "$SRC" "$backup_dir"
+    echo "succes remote" 
+    else
+    rsync -avz --delete --link-dest="$TGT/latest" "$SRC" "$backup_dir"
+    echo "succes local" 
+    fi
 }
-
-# Check if two arguments are provided
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <source_directory> <target_directory>"
-    exit 1
-fi
-
 # Check if source directory exists
 if [ ! -d "$SOURCE_DIR" ]; then
     echo "Invalid source directory: $SOURCE_DIR"
@@ -49,14 +47,6 @@ if [[ $TARGET_DIR == *:* ]]; then
     remote=true
 else
     remote=false
-fi
-
-# Create the target directory if it doesn't exist
-if [ $remote = false ]; then
-    mkdir -p "$TARGET_DIR" || {
-        echo "Failed to create target directory: $TARGET_DIR"
-        exit 3
-    }
 fi
 
 # Create a timestamped backup directory
